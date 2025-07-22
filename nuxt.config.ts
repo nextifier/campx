6e93dfad9667804ca78e8f023c774d23a3c1819d
Nextifier <nextifier@gmail.com>
2025-07-22 13:13:07 +0700

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    preset: "cloudflare_module",
+    preset: "cloudflare_pages",
     cloudflare: {
       deployConfig: true,
       nodeCompat: true,
