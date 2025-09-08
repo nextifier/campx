e8b2615f79578d249a7974bc6b639859696f48e0
Nextifier <nextifier@gmail.com>
2025-09-08 16:57:03 +0700

    updates

--- server/api/sitemap-urls.ts
+++ server/api/sitemap-urls.ts
@@ -1,22 +1,19 @@
 import { asSitemapUrl, defineSitemapEventHandler } from "#imports";
 
 export default defineSitemapEventHandler(async () => {
-  // const config = useRuntimeConfig();
-  // const username = "campx";
-
-  // const posts = await $fetch<ReturnType<typeof asSitemapUrl>>(
-  //   `https://blog.levenium.com/ghost/api/content/posts/?key=6516c0eb11471899a2344b81e3&filter=authors.slug:[${username}]%2Bvisibility:public&order=published_at+desc&limit=all`,
-  // ).then((res) => {
-  //   return res.posts.map((post) => {
-  //     return {
-  //       loc: `/news/${post.slug}`,
-  //       lastmod: post.updated_at,
-  //     };
-  //   });
-  // });
+  const posts = await $fetch<ReturnType<typeof asSitemapUrl>>(
+    `${useAppConfig().app.blogApiUrl}/posts/?key=${useAppConfig().app.blogApiKey}&filter=authors.slug:[${useAppConfig().app.blogUsername}]%2Bvisibility:public&order=published_at+desc&limit=all`,
+  ).then((res) => {
+    return res.posts.map((post) => {
+      return {
+        loc: `/news/${post.slug}`,
+        lastmod: post.updated_at,
+      };
+    });
+  });
 
   // const brands = await $fetch<ReturnType<typeof asSitemapUrl>>(
-  //   `${config.public.apiUrl}/api/exhibitors?filter[is_published]=1`,
+  //   `${useAppConfig().app.apiUrl}/api/exhibitors?filter[is_published]=1`,
   // ).then((res) => {
   //   return res.map((brand) => {
   //     return {
@@ -27,7 +24,7 @@ export default defineSitemapEventHandler(async () => {
   // });
 
   return [
-    // ...posts,
+    ...posts,
     // ...brands
   ];
 });
